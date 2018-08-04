e14c78ce12ce8e5d2dfb922b8513b8b158bb680c
Nat Welch <nat@natwelch.com>
2018-08-04 21:02:25 +0000

    sure

--- server/server.go
+++ server/server.go
@@ -1,26 +1,17 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"log"
 	"net/http"
-	"runtime/debug"
 
 	"github.com/99designs/gqlgen/handler"
 	"github.com/icco/writing"
 )
 
 func main() {
-	http.Handle("/", handler.Playground("Todo", "/query"))
+	http.Handle("/", handler.Playground("writing", "/query"))
 	http.Handle("/query", handler.GraphQL(
-		writer.NewExecutableSchema(todo.New()),
-		handler.RecoverFunc(func(ctx context.Context, err interface{}) error {
-			// send this panic somewhere
-			log.Print(err)
-			debug.PrintStack()
-			return errors.New("user message on panic")
-		}),
+		writing.NewExecutableSchema(writing.New()),
 	))
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
