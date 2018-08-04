d3a1873c8ccc5e0673af3eec6cca57b0e3a17218
Nat Welch <nat@natwelch.com>
2018-08-04 23:07:33 +0000

    fix some queries

--- resolver.go
+++ resolver.go
@@ -31,7 +31,7 @@ type mutationResolver struct{ *Resolver }
 func (r *mutationResolver) CreatePost(ctx context.Context, input NewPost) (Post, error) {
 	panic("not implemented")
 }
-func (r *mutationResolver) EditPost(ctx context.Context, Id string, input NewPost) (Post, error) {
+func (r *mutationResolver) EditPost(ctx context.Context, id string, input NewPost) (Post, error) {
 	panic("not implemented")
 }
 func (r *mutationResolver) CreateLink(ctx context.Context, input NewLink) (Link, error) {
@@ -64,7 +64,7 @@ func (r *queryResolver) AllPosts(ctx context.Context) ([]*Post, error) {
 }
 
 func (r *queryResolver) Posts(ctx context.Context, limit *int, offset *int) ([]*Post, error) {
-	rows, err := db.Query("SELECT id, title, content, date, created_at, modified_at, tags, draft FROM posts WHERE draft = false ORDER BY date DESC LIMIT $1, $2", limit, offset)
+	rows, err := db.Query("SELECT id, title, content, date, created_at, modified_at, tags, draft FROM posts WHERE draft = false ORDER BY date DESC LIMIT $1 OFFSET $2", limit, offset)
 	if err != nil {
 		return nil, err
 	}
