4d04e9481ed3a6c33df663e0fa44c3d220ec9d1e
Nat Welch <nat@natwelch.com>
2018-08-05 06:15:40 +0000

    Add cors

--- server/server.go
+++ server/server.go
@@ -12,7 +12,9 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/icco/graphql"
+	"github.com/rs/cors"
 	"gopkg.in/unrolled/render.v1"
+	"gopkg.in/unrolled/secure.v1"
 )
 
 // Renderer is a renderer for all occasions. These are our preferred default options.
@@ -50,6 +52,19 @@ func main() {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+
+	r.Use(cors.New(cors.Options{
+		AllowCredentials:   true,
+		OptionsPassthrough: true,
+	}).Handler)
+
+	r.Use(secure.New(secure.Options{
+		HostsProxyHeaders:  []string{"X-Forwarded-Host"},
+		FrameDeny:          true,
+		ContentTypeNosniff: true,
+		BrowserXssFilter:   true,
+		IsDevelopment:      false,
+	}).Handler)
 
 	r.Get("/healthz", healthCheckHandler)
 
