0ce4e9b6c60ffe08151f942da7604a977c753eb6
Nat Welch <nat@natwelch.com>
2018-09-04 12:45:35 +0000

    style tweaks

--- resolver.go
+++ resolver.go
@@ -29,18 +29,14 @@ func (r *Resolver) Query() QueryResolver {
 
 type mutationResolver struct{ *Resolver }
 
-func GetMaxId() (int, error) {
-	row, err := db.QueryRow("SELECT MAX(id) from posts")
-	if err != nil {
-		return err
-	}
-
-	var id int
+func GetMaxId() (int64, error) {
+	row := db.QueryRow("SELECT MAX(id) from posts")
+	var id int64
 	if err := row.Scan(&id); err != nil {
-		return err
+		return -1, err
 	}
 
-	return id
+	return id, nil
 }
 
 func (r *mutationResolver) CreatePost(ctx context.Context, input NewPost) (Post, error) {
@@ -51,7 +47,7 @@ func (r *mutationResolver) CreatePost(ctx context.Context, input NewPost) (Post,
 	}
 	id := maxId + 1
 
-	result, err := db.Exec("INSERT INTO posts(id, title, content, date, draft, created_at, modified_at) VALUES ($1, $2, $3, $4, $5, $6, $6)",
+	_, err = db.Exec("INSERT INTO posts(id, title, content, date, draft, created_at, modified_at) VALUES ($1, $2, $3, $4, $5, $6, $6)",
 		id,
 		input.Title,
 		input.Content,
