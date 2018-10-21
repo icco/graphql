33167f811685d1a5abd25fc927874e764a7f2ac4
Nat Welch <nat@natwelch.com>
2018-10-21 01:54:23 +0000

    small lint tweak

--- sanitizer.go
+++ sanitizer.go
@@ -5,9 +5,10 @@ import (
 	"strings"
 )
 
-// sanitize will replace existing backticks with escape chars, then wrap in backticks.
+// sanitize will replace existing backticks with escape chars, then wrap in
+// backticks.
 func sanitize(str string) string {
 	withEscapedBackticks := strings.Replace(str, "`", "\\`", -1)
 	wrappedSanitizedString := fmt.Sprintf("`%s`", withEscapedBackticks)
-	return wrappedSanitizedString;
+	return wrappedSanitizedString
 }
