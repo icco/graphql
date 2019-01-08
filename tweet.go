2621ba2b46a236f4d78fa6f5fad404f95d694f96
Nat Welch <nat@natwelch.com>
2019-01-08 11:33:29 +0000

    insert code

--- tweet.go
+++ tweet.go
@@ -3,6 +3,8 @@ package graphql
 import (
 	"context"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 // A Tweet is an archived tweet.
@@ -14,9 +16,38 @@ type Tweet struct {
 	UserMentions  []string  `json:"user_mentions"`
 	Urls          []string  `json:"urls"`
 	User          string    `json:"user"`
-	FavoriteCount int       `json:"favorite_count"`
-	RetweetCount  int       `json:"retweet_count"`
+	FavoriteCount int64     `json:"favorite_count"`
+	RetweetCount  int64     `json:"retweet_count"`
 	Posted        time.Time `json:"posted"`
+}
+
+// Save inserts or updates a tweet into the database.
+func (t *Tweet) Save(ctx context.Context) error {
+	if _, err := db.ExecContext(
+		ctx,
+		`
+INSERT INTO tweets(id, text, hashtags, symbols, user_mentions, urls, user, favorites, retweets, posted, created_at, modified_at)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $10)
+ON CONFLICT (id) DO UPDATE
+SET (text, hashtags, symbols, user_mentions, urls, user, favorites, retweets, posted, modified_at) = ($2, $3, $4, $5, $6, $7, $8, $9, $10)
+WHERE tweets.id = $1;
+`,
+		t.ID,
+		t.Text,
+		pq.Array(t.Hashtags),
+		pq.Array(t.Symbols),
+		pq.Array(t.UserMentions),
+		pq.Array(t.Urls),
+		t.User,
+		t.FavoriteCount,
+		t.RetweetCount,
+		t.Posted,
+		time.Now(),
+	); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func GetTweets(ctx context.Context, limit *int, offset *int) ([]*Tweet, error) {
