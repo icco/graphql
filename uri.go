fb32e980a655652d15c7a640ca7a5c85d8b831e4
Nat Welch <nat@natwelch.com>
2019-03-13 03:32:27 +0000

    lint

--- uri.go
+++ uri.go
@@ -6,19 +6,23 @@ import (
 	"io"
 )
 
+// URI is a string representation of a URI.
 // TODO: Turn into an actual URI.
 type URI struct {
 	value string
 }
 
+// String returns the value
 func (u *URI) String() string {
 	return u.value
 }
 
+// Scan implements the driver.Scan interface
 func (u *URI) Scan(v interface{}) error {
 	return u.UnmarshalGQL(v)
 }
 
+// UnmarshalGQL implements the graphql.Marshaler interface
 func (u *URI) UnmarshalGQL(v interface{}) error {
 	if v == nil {
 		u.value = ""
@@ -39,6 +43,7 @@ func (u URI) MarshalGQL(w io.Writer) {
 	fmt.Fprintf(w, `"%s"`, u.String())
 }
 
+// Value implements the driver.Value interface
 func (u URI) Value() (driver.Value, error) {
 	return u.value, nil
 }
