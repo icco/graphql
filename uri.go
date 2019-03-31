ebba5bdf16f0d2a39238fc19fbe6ad6b8959f4ad
Nat Welch <nat@natwelch.com>
2019-03-31 02:20:40 +0000

    simplify uri

--- uri.go
+++ uri.go
@@ -9,18 +9,18 @@ import (
 // URI is a string representation of a URI.
 // TODO: Turn into an actual URI.
 type URI struct {
-	Raw string
+	raw string
 }
 
 func NewURI(raw string) URI {
 	u := URI{}
-	u.Raw = raw
+	u.raw = raw
 	return u
 }
 
 // String returns the value
 func (u *URI) String() string {
-	return u.Raw
+	return u.raw
 }
 
 // Scan implements the driver.Scan interface
@@ -31,7 +31,7 @@ func (u *URI) Scan(v interface{}) error {
 // UnmarshalGQL implements the graphql.Marshaler interface
 func (u *URI) UnmarshalGQL(v interface{}) error {
 	if v == nil {
-		u.Raw = ""
+		u.raw = ""
 		return nil
 	}
 
@@ -39,7 +39,7 @@ func (u *URI) UnmarshalGQL(v interface{}) error {
 	if !ok {
 		return fmt.Errorf("URI must be strings")
 	}
-	u.Raw = str
+	u.raw = str
 
 	return nil
 }
@@ -51,5 +51,5 @@ func (u URI) MarshalGQL(w io.Writer) {
 
 // Value implements the driver.Value interface
 func (u URI) Value() (driver.Value, error) {
-	return u.Raw, nil
+	return u.raw, nil
 }
