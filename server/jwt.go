0f0f0575fa1ec9c3e842c0930bc9d1c1a06d50ee
Nat Welch <nat@natwelch.com>
2020-01-09 02:54:37 +0000

    progress

--- server/jwt.go
+++ server/jwt.go
@@ -4,9 +4,9 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/auth0-community/go-auth0"
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/icco/graphql"
-	jose "gopkg.in/square/go-jose.v2"
 )
 
 var (
@@ -18,11 +18,31 @@ var (
 	}
 )
 
-// AuthMiddleware parses the incomming authentication header and turns it into
-// an attached user.
-func AuthMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// Jwks is from https://auth0.com/docs/quickstart/backend/golang
+type Jwks struct {
+	Keys []JSONWebKeys `json:"keys"`
+}
 
+// JSONWebKeys is from https://auth0.com/docs/quickstart/backend/golang
+type JSONWebKeys struct {
+	Kty string   `json:"kty"`
+	Kid string   `json:"kid"`
+	Use string   `json:"use"`
+	N   string   `json:"n"`
+	E   string   `json:"e"`
+	X5c []string `json:"x5c"`
+}
+
+// CustomClaims is from https://auth0.com/docs/quickstart/backend/golang
+type CustomClaims struct {
+	Scope string `json:"scope"`
+	jwt.StandardClaims
+}
+
+// APIKeyMiddleware is an auth middleware. If user is coming in via api key
+// header, use that as your auth.
+func APIKeyMiddleware(next http.handler) http.handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// API Key dropout
 		if r.Header.Get("X-API-AUTH") != "" {
 			apikey := r.Header.Get("X-API-AUTH")
@@ -36,50 +56,94 @@ func AuthMiddleware(next http.Handler) http.Handler {
 			// put it in context
 			ctx := graphql.WithUser(r.Context(), user)
 			r = r.WithContext(ctx)
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		secretProvider := auth0.NewJWKClient(auth0.JWKClientOptions{URI: AUTH0["DOMAIN"] + "/.well-known/jwks.json"}, nil)
-		audience := []string{AUTH0["API-AUDIENCE"]}
-		configuration := auth0.NewConfiguration(secretProvider, audience, AUTH0["DOMAIN"]+"/", jose.RS256)
-		validator := auth0.NewValidator(configuration, nil)
-
-		token, err := validator.ValidateRequest(r)
-
-		// This also returns an error if no token
-		if err != nil {
-			if err.Error() == "Token not found" {
-				next.ServeHTTP(w, r)
-				return
-			}
-
-			log.WithField("auth", AUTH0).WithError(err).Error("token is not valid")
-			http.Error(w, `{"error": "Error reading auth token"}`, http.StatusBadRequest)
-			return
-		}
-
-		claims := map[string]interface{}{}
-		err = validator.Claims(r, token, &claims)
-		if err != nil {
-			log.WithError(err).Error("Claims are not valid")
-			http.Error(w, http.StatusText(403), 403)
-			return
-		}
-
-		// According to Auth0, the sub claim is what we're supposed to use to identify a unique user... so here we go!
-		userid, ok := claims["sub"].(string)
-		if ok {
-			user, err := graphql.GetUser(r.Context(), userid)
-			if err != nil {
-				log.WithError(err).WithField("claims", claims).Error("could not get user")
-			} else {
-				// put it in context
-				ctx := graphql.WithUser(r.Context(), user)
-				r = r.WithContext(ctx)
-			}
 		}
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+// AuthMiddleware parses the incomming authentication header and turns it into
+// an attached user.
+func AuthMiddleware(next http.handler) http.handler {
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			// Verify 'aud' claim
+			aud := "https://natwelch.com"
+			checkAud := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+			if !checkAud {
+				return token, errors.New("Invalid audience.")
+			}
+			// Verify 'iss' claim
+			iss := "https://natwelch.com/"
+			checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+			if !checkIss {
+				return token, errors.New("Invalid issuer.")
+			}
+
+			cert, err := getPemCert(token)
+			if err != nil {
+				log.WithError(err).Error("cloudn't parse pem cert")
+			}
+
+			return jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+		},
+		SigningMethod: jwt.SigningMethodRS256,
+	})
+
+	return jwtMiddleware.Handler(next)
+}
+
+func getPemCert(token *jwt.Token) (string, error) {
+	cert := ""
+	resp, err := http.Get("https://natwelch.com/.well-known/jwks.json")
+
+	if err != nil {
+		return cert, err
+	}
+	defer resp.Body.Close()
+
+	var jwks = Jwks{}
+	err = json.NewDecoder(resp.Body).Decode(&jwks)
+
+	if err != nil {
+		return cert, err
+	}
+
+	for k, _ := range jwks.Keys {
+		if token.Header["kid"] == jwks.Keys[k].Kid {
+			cert = "-----BEGIN CERTIFICATE-----\n" + jwks.Keys[k].X5c[0] + "\n-----END CERTIFICATE-----"
+		}
+	}
+
+	if cert == "" {
+		err := errors.New("Unable to find appropriate key.")
+		return cert, err
+	}
+
+	return cert, nil
+}
+
+func checkScope(scope string, tokenString string) bool {
+	token, _ := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		cert, err := getPemCert(token)
+		if err != nil {
+			return nil, err
+		}
+		result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+		return result, nil
+	})
+
+	claims, ok := token.Claims.(*CustomClaims)
+
+	hasScope := false
+	if ok && token.Valid {
+		result := strings.Split(claims.Scope, " ")
+		for i := range result {
+			if result[i] == scope {
+				hasScope = true
+			}
+		}
+	}
+
+	return hasScope
 }
