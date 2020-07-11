8d6a92b9f97bcc7f4ac728c60f9daa846d94f566
Nat Welch <nat@natwelch.com>
2020-07-11 16:19:34 +0000

    generate

--- blog.resolvers.go
+++ blog.resolvers.go
@@ -91,6 +91,10 @@ func (r *queryResolver) Posts(ctx context.Context, input *Limit) ([]*Post, error
 	return Posts(ctx, limit, offset)
 }
 
+func (r *queryResolver) Comments(ctx context.Context, input *Limit) ([]*Comment, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
 func (r *queryResolver) Search(ctx context.Context, query string, input *Limit) ([]*Post, error) {
 	limit, offset := ParseLimit(input, 10, 0)
 
