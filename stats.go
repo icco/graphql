f0a384e43d1859f40ee612018315e8d3f966c5e4
Nat Welch <nat@natwelch.com>
2020-12-21 15:27:17 +0000

    Add save

--- stats.go
+++ stats.go
@@ -6,7 +6,28 @@ import (
 )
 
 func (s *Stat) Save(ctx context.Context) error {
-	return fmt.Errorf("not implemented")
+	if s.Key == "" {
+		return fmt.Errorf("Empty key not allowed")
+	}
+
+	s.Modified = time.Now()
+
+	if _, err := db.ExecContext(
+		ctx,
+		`
+INSERT INTO stats(key, value, modified_at)
+VALUES ($1, $2, $3)
+ON CONFLICT (key) DO UPDATE
+SET (value, modified_at) = ($2, $3)
+WHERE stats.key = $1;
+`,
+		s.Key,
+		s.Value,
+		s.Modified); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func GetStats(ctx context.Context, limit int) ([]*Stat, error) {
