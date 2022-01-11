c61bd3ee2c8130c6d742bab8032f4a74797a3466
Nat Welch <nat@natwelch.com>
2022-01-11 13:15:25 +0000

    params

--- imgix.go
+++ imgix.go
@@ -9,10 +9,32 @@ import (
 	ix "github.com/imgix/imgix-go/v2"
 )
 
-// https://assets.imgix.net/page-weight/canvas.png?bg=eeeceb&mark64=aHR0cHM6Ly9uYXR3ZWxjaC5jb20vaS9sb2dvLnBuZz9oPTEwMA&w=1200&h=630&fit=crop&bm=normal&bx=50&ba=bottom&markx=40&txt64=MjAyMi0wMS0xMA&txtalign=left%2Cbottom&txtsize=24&txtclr=000000&txtpad=60&txtclip=end%2Cellipsis&by=280&txtfont64=RGluIEFsdGVybmF0ZQ&fm=png8&marky=28&mark-w=200
+// GenerateSocialImage creates a static image URL for a post.
 func GenerateSocialImage(ctx context.Context, title string, when time.Time) (*URI, error) {
 	ixToken := os.Getenv("IX_TOKEN")
-	ix.NewURLBuilder("icco.imgix.net", ix.WithToken(ixToken))
-	// canvas.png
-	return nil, fmt.Errorf("not implemented")
+	ub := ix.NewURLBuilder("icco.imgix.net", ix.WithToken(ixToken))
+	urlString := ub.CreateURL("/canvas.png", []ix.IxParam{
+		ix.Param("bg", "eeeceb"),
+		ix.Param("mark64", "aHR0cHM6Ly9uYXR3ZWxjaC5jb20vaS9sb2dvLnBuZz9oPTEwMA"),
+		ix.Param("w", "1200"),
+		ix.Param("h", "630"),
+		ix.Param("fit", "crop"),
+		ix.Param("bm", "normal"),
+		ix.Param("bx", "50"),
+		ix.Param("ba", "bottom"),
+		ix.Param("markx", "40"),
+		ix.Param("txt64", "MjAyMi0wMS0xMA"),
+		ix.Param("txtalign", "left%2Cbottom"),
+		ix.Param("txtsize", "24"),
+		ix.Param("txtclr", "000000"),
+		ix.Param("txtpad", "60"),
+		ix.Param("txtclip", "end%2Cellipsis"),
+		ix.Param("by", "280"),
+		ix.Param("txtfont64", "RGluIEFsdGVybmF0ZQ"),
+		ix.Param("fm", "png8"),
+		ix.Param("marky", "28"),
+		ix.Param("mark-w", "200"),
+	}...)
+
+	return NewURI(urlString)
 }
